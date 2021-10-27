c18833fe120a220e299ccf72af92d7e38e9fba11
João Victor Pacheco Ferreira Silva <48071158801@info.local>
2021-10-27 11:10:30 -0300

    Areas atualizadas

--- src/pages/Areas/areas.js
+++ src/pages/Areas/areas.js
@@ -8,28 +8,29 @@ export const Areas = () => {
         <>
             <Header/>
             <main>
-                <section className="banner">
+                <div className="banner">
                     <div className="imagem-area">
                     </div>
-                    <h1>
-                        Nosso plano de carreira 
-                    </h1>
-                    <h2>
-                        Vamos juntpos em direção ao sucesso
-                    </h2>
-                    <p>
-                    aqui mostramos o caminho que você pode trilhar para alcançar seus objetivos dentro da empresa
-                    </p>
-                
-                </section>
+                    <div className="texto-banner">
+                        <h1>
+                            Nosso plano de carreira 
+                        </h1>
+                        <h2>
+                            Vamos juntos em direção ao sucesso
+                        </h2>
+                        <p>
+                        aqui mostramos o caminho que você pode trilhar para <br/> alcançar seus objetivos dentro da empresa
+                        </p>
+                    </div>
+                </div>
                 <div className="texto">
                     <div className="texto-info">
                         <h1>
                         Trilhagem de carreira
                         </h1>
-                        <p>
+                        <h2>
                         Muitas vezes nos perguntamos como chegar em alguma posição hierarquicamente superior em nossa empresa, ou até como mudar de foco, procurando outras áreas. Esse processo pode demandar muito tempo de organização e planejamento, mas com a trilhagem de carreira todo esse processo se torna mais fácil, já que mostramos o caminho a ser seguido para alcançar o seu objetivo na empresa. 
-                        </p>
+                        </h2>
                     </div>
                 </div>
                 
