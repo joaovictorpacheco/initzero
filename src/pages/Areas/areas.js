c1c97c26e36fda71295513d8218abf61fae00683
João Victor Pacheco <joaovictorspzo@gmail.com>
2021-11-02 22:55:20 -0300

    Alteraçoes

--- src/pages/Areas/areas.js
+++ src/pages/Areas/areas.js
@@ -24,14 +24,26 @@ export const Areas = () => {
                     </div>
                 </div>
                 <div className="texto">
-                    <div className="texto-info">
                         <h1>
                         Trilhagem de carreira
                         </h1>
+                    <div className="texto-info">
+                        
                         <h2>
                         Muitas vezes nos perguntamos como chegar em alguma posição hierarquicamente superior em nossa empresa, ou até como mudar de foco, procurando outras áreas. Esse processo pode demandar muito tempo de organização e planejamento, mas com a trilhagem de carreira todo esse processo se torna mais fácil, já que mostramos o caminho a ser seguido para alcançar o seu objetivo na empresa. 
                         </h2>
+                                            
                     </div>
+                </div>
+                <div className="conhecimento">
+                    <h1>
+                        Áreas do conhecimento na nossa empresa
+                    </h1>
+                        <div className="texto-conhecimento">
+                            <h2>
+                            Essas são todas as áreas de dentro de nossa empresa. A partir de uma área há diversas trilhas a serem seguidas, além de muitas etapas a serem completadas. Escolha a de seu interesse: 
+                            </h2>
+                        </div>                    
                 </div>
                 
             </main>
